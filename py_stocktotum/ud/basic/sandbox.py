d72742b1dd5433a805e24aa53d3d9c72cf9bd9b9
rjasiak54 <rjasiak54@gmail.com>
2025-02-25 22:48:10 -0600

    added sandbox script

--- py_stocktotum/ud/basic/sandbox.py
+++ py_stocktotum/ud/basic/sandbox.py
@@ -1,10 +1,12 @@
+import math
+
 from . import basic
 
 
 def main() -> None:
-    m = 100
-    r = 0.05
-    y = 5
+    m = 1000
+    r = 0.04
+    y = 3
     result = basic.future_discrete_value(m, r, y)
     print(f'fdv:  {m} * (1 + {r})^{y}   = {result}')
 
@@ -17,7 +19,12 @@ def main() -> None:
     result = basic.present_continuous_value(m, r, y)
     print(f'pcv:  {m} * e^-rt           = {result}')
 
+    result = (100/0.04) * (1 - (1 / (1.04 ** 3))) + (1000 / (1.04 ** 3))
+    print(result)
+    print()
 
+    _e = sum([1 / math.factorial(i) for i in range(100)])
+    print(f'Value of e: {_e}')
 
 if __name__ == '__main__':
     main()
