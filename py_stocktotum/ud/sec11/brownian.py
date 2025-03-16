1fea6a09df60f7751f4fcae230e813cb5249e76d
rjasiak54 <rjasiak54@gmail.com>
2025-03-15 21:29:05 -0500

    sec 11

--- py_stocktotum/ud/sec11/brownian.py
+++ py_stocktotum/ud/sec11/brownian.py
@@ -14,7 +14,7 @@ def simulate_geometric_random_walk(
     dt = T / N
     t = np.linspace(0, T, N)
     W = np.random.standard_normal(size=N)
-    W = np.cumsum(W) * np.sqrt(dt)
+    W = np.cumsum(W)  # * np.sqrt(dt)
     X = (mu - 0.5 * sigma**2) * t + sigma * W
     S = S0 * np.exp(X)
     return t, S
