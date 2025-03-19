7f74e04c4f615d5b8ffc9c62d4a7af02b81cfdee
rjasiak54 <rjasiak54@gmail.com>
2025-03-18 22:03:14 -0500

    started on sma

--- py_stocktotum/common/yf.py
+++ py_stocktotum/common/yf.py
@@ -7,10 +7,13 @@ import yfinance as yf  # type: ignore
 
 def download_data(
     stock: str,
-    start_date: datetime.datetime,
-    end_date: datetime.datetime,
+    start_date: datetime.datetime | str,
+    end_date: datetime.datetime | str,
 ) -> pd.DataFrame:
     ticker = yf.download(stock, start_date, end_date)
     if ticker is None:
         raise ValueError(f"Ticker is None!!! ticker={stock}")
-    return ticker["Close"]  # type: ignore
+    ret = pd.DataFrame()
+    ret["Price"] = ticker["Close"]
+
+    return ret
---
+++ py_stocktotum/ud_q/sma.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt
+
+from py_stocktotum import common
+
+if __name__ == "__main__":
+    start = "2010-01-01"
+    end = "2020-01-01"
+    sd = common.yf.download_data("IBM", start, end)
+    print(sd)
+    plt.plot(sd)
+    plt.show()
