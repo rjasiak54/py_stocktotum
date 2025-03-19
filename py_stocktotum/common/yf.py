1c5b2ff017f963991fc1871db6558440efcf97c7
rjasiak54 <rjasiak54@gmail.com>
2025-03-18 21:37:31 -0500

    added python stuff

---
+++ py_stocktotum/common/yf.py
@@ -0,0 +1,16 @@
+import datetime
+
+import numpy as np
+import pandas as pd
+import yfinance as yf  # type: ignore
+
+
+def download_data(
+    stock: str,
+    start_date: datetime.datetime,
+    end_date: datetime.datetime,
+) -> pd.DataFrame:
+    ticker = yf.download(stock, start_date, end_date)
+    if ticker is None:
+        raise ValueError(f"Ticker is None!!! ticker={stock}")
+    return ticker["Close"]
